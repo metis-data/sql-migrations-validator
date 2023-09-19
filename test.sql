c5252a17a69fda7beb581a5784dad297e5f38137
gideons <gideons@metisdata.io>
2023-09-19 15:17:01 +0300

    update test sql for checking new insights changes

--- test.sql
+++ test.sql
@@ -1,4 +1,18 @@
+DROP TABLE "test_table";
 
-ALTER TABLE orders DROP CONSTRAINT fk_orders_customers;
+DROP INDEX "test_workflow_key";
 
-DROP INDEX some_index CASCADE;
+ALTER TABLE "test_workflow"
+DROP COLUMN "test_name",
+ADD COLUMN "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN "pr_id" TEXT NOT NULL,
+ADD COLUMN "pr_name" TEXT NOT NULL
+
+CREATE INDEX index_name ON test_table (column1, column2);
+DROP TABLE test_table;
+ALTER TABLE test_table DROP CONSTRAINT fk_test_table;
+ALTER TABLE tbl_name  DROP COLUMN col_name;
+DROP INDEX test_index;
+ALTER TABLE test_table RENAME COLUMN column_name TO new_column_name;
+ALTER TABLE test_table RENAME TO new_test_table;
+CREATE INDEX index_name ON test_table (column1, column2);
